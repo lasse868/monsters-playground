d7735a4d6cad72432b0cb9b6dc8354f8c13725c0
Lars Malnig <mlars.lasse@gmail.com>
2024-08-16 18:39:04 +0200

    changed to monster searching app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 const App=()=> {
-  const [name, setName]=useState("Andrei");
+
+  const [monsters, setMonsters]=useState([]);
+  const [searchField, setSearchField]=useState("");
+  const [filteredMonsters, setfilteredMonsters]=useState(monsters);
+
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response)=>response.json())
+    .then((users)=>setMonsters(users))
+  },[monsters])
+
+  const onSearchChange=(event)=>{
+    const searchString=event.target.value.toLowerCase();
+    setSearchField(searchString);
+  }
+
+  useEffect(()=>{
+    const newFilteredMonsters=monsters.filter((monster)=>(
+      monster.name.toLowerCase().includes(searchField)
+    ));
+    setfilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi {name}</p>
-        <button onClick={()=>(setName("Yihua"))}>Change Name</button>
-      </header>
+      <h1>Monster Rolodex</h1>
+
+      <input type='search' placeholder='search monster' onChange={onSearchChange}/>
+
+      {
+        filteredMonsters.map((monster)=>{
+          return(
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+          )
+        })
+      }
+
     </div>
   );
 }
